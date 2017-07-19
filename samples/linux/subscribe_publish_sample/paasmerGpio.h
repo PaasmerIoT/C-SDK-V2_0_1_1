8ddd3f3bc56bcd052a0d80fc7e80e4fec05949b9
Paasmer <paasmer654@gmail.com>
2017-07-19 14:45:32 +0530

    Update paasmerGpio.h

--- samples/linux/subscribe_publish_sample/paasmerGpio.h
+++ samples/linux/subscribe_publish_sample/paasmerGpio.h
@@ -1,31 +1,20 @@
 #include "device.h"
 #include <stdio.h>
-//#include <../../../../wiringPi/wiringPi/wiringPi.h>
-
-
-//#define OUT 2
-//#define IN 4
-//#define HIGH 2
-//#define LOW 4
-//#define deviceType
 char command[100];
 
 void gpioSetup()
 {
-	if((strcmp(deviceType,"raspberrypi")) || (strcmp(deviceType,"bananapi")) || (strcmp(deviceType,"orangepi")) || (strcmp(deviceType,"odroidxu4")))
-	//if(deviceType == "Raspberry")
+	if((!strcmp(deviceType,"raspberrypi")) || (!strcmp(deviceType,"bananapi")) || (!strcmp(deviceType,"orangepi")) || (!strcmp(deviceType,"odroidxu4")))
 	{
-		//wiringPiSetup();
+		
 	}
 	else;
 }
 
 void gpioModesetup(int pinNum,char *mode)
 {
-	//if(strcmp(deviceType,"raspberry"))
-	if((strcmp(deviceType,"raspberrypi")) || (strcmp(deviceType,"bananapi")) || (strcmp(deviceType,"orangepi")) || (strcmp(deviceType,"odroidxu4")))
+	if((!strcmp(deviceType,"raspberrypi")) || (!strcmp(deviceType,"bananapi")) || (!strcmp(deviceType,"orangepi")) || (!strcmp(deviceType,"odroidxu4")))
 	{
-		//pinMode(pinNum,mode);
 		if(mode == "OUT")
 		{
 			sprintf(command,"gpio -1 mode %d out",pinNum);
@@ -45,10 +34,8 @@ void gpioModesetup(int pinNum,char *mode)
 
 void gpioWrite(int pinNum,int state)
 {
-	//if(strcmp(deviceType,"raspberry"))
-	if((strcmp(deviceType,"raspberrypi")) || (strcmp(deviceType,"bananapi")) || (strcmp(deviceType,"orangepi")) || (strcmp(deviceType,"odroidxu4")))
+	if((!strcmp(deviceType,"raspberrypi")) || (!strcmp(deviceType,"bananapi")) || (!strcmp(deviceType,"orangepi")) || (!strcmp(deviceType,"odroidxu4")))
 	{
-		//digitalWrite(pinNum,state);
 		sprintf(command,"gpio -1 write %d %d",pinNum,state);
 		system(command);
 		strcpy(command,"\0");
@@ -59,19 +46,15 @@ void gpioWrite(int pinNum,int state)
 int gpioRead(int pinNum)
 {
 	int res=0;
-	if((strcmp(deviceType,"raspberrypi")) || (strcmp(deviceType,"bananapi")) || (strcmp(deviceType,"orangepi")) || (strcmp(deviceType,"odroidxu4")))
-	//if(strcmp(deviceType,"Raspberry"))
-        {
-		//res = digitalRead(pinNum);
+	if((!strcmp(deviceType,"raspberrypi")) || (!strcmp(deviceType,"bananapi")) || (!strcmp(deviceType,"orangepi")) || (!strcmp(deviceType,"odroidxu4")))
+	{
 		sprintf(command,"gpio -1 read %d",pinNum);
-		//res = system(command);
 		FILE *fp = popen(command,"r");
 		char data[10];
 		fgets(data,10,fp);
 		pclose(fp);
 		strcpy(command,"\0");
 		res = atoi(data);
-		//printf("%d",res);
 	}
 	else;
 	return res;
